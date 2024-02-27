49953769ebeeaa0961587cbba71131f53dc2a466
Uddinrazi <uddinrazi88@gmail.com>
2024-02-27 17:54:08 +0530

    some modification done

--- src/components/Products/ProductList.js
+++ src/components/Products/ProductList.js
@@ -1,37 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Button from "../UI/Button";
 import classes from "./ProductList.module.css";
 import Card from "../UI/Card";
 
-// const getProductsFromLocalStorage = () => {
-//   const storedProducts = Object.keys(localStorage).map((key) =>
-//     JSON.parse(localStorage.getItem(key))
-//   );
-//   console.log(storedProducts);
-//   if (storedProducts) {
-//     return storedProducts;
-//   }
-// };
+
 const ProductList = (props) => {
-  // const [products, setUpdatedProducts] = useState(props.product);
 
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    // Function to retrieve products from local storage
-    const getProductsFromLocalStorage = () => {
-      const storedProducts = Object.keys(localStorage).map((key) =>
-        JSON.parse(localStorage.getItem(key))
-      );
-      //console.log(storedProducts);
-      if (storedProducts) {
-        setProducts(storedProducts);
-      }
-    };
-
-    // Call the function to retrieve products from local storage when the component mounts
-    getProductsFromLocalStorage();
-  }, []);
 
   const totalPrice = props.products.reduce((total, product) => {
     const price = parseFloat(product.price);
